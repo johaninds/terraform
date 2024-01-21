cb9eb6727b9013689f782721c2b918a5ea5e4542
johaninds <115527734+johaninds@users.noreply.github.com>
2024-01-21 19:46:24 +0700

    terra eks worked

--- terraform-eks/terraform.tf
+++ terraform-eks/terraform.tf
@@ -1,35 +1,35 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.46.0"
     }
 
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.4.3"
     }
 
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "~> 4.0.4"
     }
 
     cloudinit = {
-      source = "hashicorp/cloudinit"
+      source  = "hashicorp/cloudinit"
       version = "~> 2.2.0"
     }
 
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "~> 2.16.1"
     }
   }
 
   backend "s3" {
-    bucket         	   = "terra-eks-johan"
-    key              	   = "state/terraform.tfstate"
-    region         	   = "us-east-1"
+    bucket = "terra-eks-johan"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.3"
--- terraform-eks/variables.tf
+++ terraform-eks/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "AWS region"
- type = string
- default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
- type = string
- default = "vpro-johan-eks"
+  type        = string
+  default     = "vpro-johan-eks"
 }
